a99ce9de7c95663e83cd919a475b82982955c5d8
Mahati Gollamudi <github email address>
2016-08-08 17:20:03 -0400

    Create Table and Migrate

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration
+
+  def change
+    create_table :costume_stores do |t|
+      t.string :name, :location
+      t.integer :costume_inventory, :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time, :closing_time
+      t.timestamps null: false
+    end
+
+  end
+  
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+
+      t.string :name, :location, :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :closing_date,  :opening_date
+      t.string :description
+      t.timestamps null: false
+    end
+
+  end
+
+end
